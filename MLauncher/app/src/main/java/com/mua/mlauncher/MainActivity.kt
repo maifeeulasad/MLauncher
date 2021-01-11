17a5b42935ea00c416a1558e6b84613f4349120f
maifeeulasad <maifeeulasad@gmail.com>
2021-01-11 14:30:29 +0600

    home launcher + no action bar

--- MLauncher/app/src/main/java/com/mua/mlauncher/MainActivity.kt
+++ MLauncher/app/src/main/java/com/mua/mlauncher/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,6 +17,8 @@ class MainActivity : AppCompatActivity(), ApplicationClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
         val appUsageListAdapter = ApplicationListAdapter(this)
         val appUsageRecyclerView: RecyclerView = findViewById(R.id.rv_apps)
         appUsageRecyclerView.adapter = appUsageListAdapter
@@ -28,17 +30,24 @@ class MainActivity : AppCompatActivity(), ApplicationClickListener {
     }
 
     private fun listAllApplication(): MutableList<com.mua.mlauncher.ApplicationInfo> {
-        val applications : MutableList<com.mua.mlauncher.ApplicationInfo> = ArrayList()
-        val packageManager = packageManager;
+        val applications: MutableList<com.mua.mlauncher.ApplicationInfo> = ArrayList()
+        val packageManager = packageManager
         val appList = packageManager!!.getInstalledPackages(0)
         for (i in appList.indices) {
             val packageInfo = appList[i]
             if (packageInfo!!.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM == 0) {
-                val applicationName = packageInfo.applicationInfo.loadLabel(packageManager).toString()
+                val applicationName =
+                    packageInfo.applicationInfo.loadLabel(packageManager).toString()
                 val applicationDrawable = packageInfo.applicationInfo.loadIcon(packageManager)
                 val applicationPackage = packageInfo.applicationInfo.packageName
 
-                applications.add(ApplicationInfo(applicationName,applicationPackage,applicationDrawable))
+                applications.add(
+                    ApplicationInfo(
+                        applicationName,
+                        applicationPackage,
+                        applicationDrawable
+                    )
+                )
             }
         }
         return applications
@@ -48,8 +57,7 @@ class MainActivity : AppCompatActivity(), ApplicationClickListener {
         startApplication(applicationInfo.applicationPackage)
     }
 
-
-    private fun startApplication(applicationPackage: String){
+    private fun startApplication(applicationPackage: String) {
         val intent: Intent
         val pm: PackageManager = packageManager
 
