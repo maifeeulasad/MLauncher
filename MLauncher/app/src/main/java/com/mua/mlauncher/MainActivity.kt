da1807b900baaf7cd269bea8844c19b723fe9d66
maifeeulasad <maifeeulasad@gmail.com>
2021-01-11 16:41:11 +0600

    viewmodel + livedata + added editext for search

--- MLauncher/app/src/main/java/com/mua/mlauncher/MainActivity.kt
+++ MLauncher/app/src/main/java/com/mua/mlauncher/MainActivity.kt
@@ -1,59 +1,53 @@
 package com.mua.mlauncher
 
 import android.content.Intent
-import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.mua.mlauncher.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity(), ApplicationClickListener {
 
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        mBinding.dashboard = viewModel
+        mBinding.lifecycleOwner = this
+
+        init()
+    }
+
+    private fun init() {
         window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 
+        initList()
+    }
+
+    private fun initList() {
         val appUsageListAdapter = ApplicationListAdapter(this)
         val appUsageRecyclerView: RecyclerView = findViewById(R.id.rv_apps)
         appUsageRecyclerView.adapter = appUsageListAdapter
         appUsageRecyclerView.layoutManager = LinearLayoutManager(this)
 
-        val allApplications = listAllApplication()
-
-        appUsageListAdapter.setApplicationList(allApplications)
+        viewModel.getApplications().observe(this, Observer { applicationList ->
+            appUsageListAdapter.setApplicationList(applicationList)
+        })
     }
 
-    private fun listAllApplication(): MutableList<com.mua.mlauncher.ApplicationInfo> {
-        val applications: MutableList<com.mua.mlauncher.ApplicationInfo> = ArrayList()
-        val packageManager = packageManager
-        val appList = packageManager!!.getInstalledPackages(0)
-        for (i in appList.indices) {
-            val packageInfo = appList[i]
-            if (packageInfo!!.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM == 0) {
-                val applicationName =
-                    packageInfo.applicationInfo.loadLabel(packageManager).toString()
-                val applicationDrawable = packageInfo.applicationInfo.loadIcon(packageManager)
-                val applicationPackage = packageInfo.applicationInfo.packageName
 
-                applications.add(
-                    ApplicationInfo(
-                        applicationName,
-                        applicationPackage,
-                        applicationDrawable
-                    )
-                )
-            }
-        }
-        return applications
-    }
-
-    override fun onApplicationClick(applicationInfo: com.mua.mlauncher.ApplicationInfo) {
+    override fun onApplicationClick(applicationInfo: ApplicationInfo) {
         startApplication(applicationInfo.applicationPackage)
     }
 
---
+++ MLauncher/app/src/main/java/com/mua/mlauncher/MainViewModel.kt
@@ -0,0 +1,46 @@
+package com.mua.mlauncher
+
+import android.app.Application
+import android.content.pm.PackageManager
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val applications = MutableLiveData(mutableListOf<ApplicationInfo>())
+
+    init {
+        listAllApplication(application.packageManager)
+    }
+
+    fun getApplications(): LiveData<MutableList<ApplicationInfo>> {
+        return applications
+    }
+
+    private fun listAllApplication(packageManager: PackageManager) {
+        val applicationList: MutableList<ApplicationInfo> = mutableListOf()
+        val appList = packageManager.getInstalledPackages(0)
+        for (i in appList.indices) {
+            val packageInfo = appList[i]
+            if (packageInfo!!.applicationInfo.flags
+                and android.content.pm.ApplicationInfo.FLAG_SYSTEM == 0
+            ) {
+                val applicationName =
+                    packageInfo.applicationInfo.loadLabel(packageManager).toString()
+                val applicationDrawable = packageInfo.applicationInfo.loadIcon(packageManager)
+                val applicationPackage = packageInfo.applicationInfo.packageName
+
+                applicationList.add(
+                    ApplicationInfo(
+                        applicationName,
+                        applicationPackage,
+                        applicationDrawable
+                    )
+                )
+            }
+        }
+        applications.postValue(applicationList)
+    }
+
+}
